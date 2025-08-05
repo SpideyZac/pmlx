0f8d55e71402d5548aed41d94eb952efd1086369
spideyzac <zacmlesser-7@outlook.com>
2025-08-04 21:17:55 -0700

    feat: woohoo indexed db auto download stuff!

--- src/registry.ts
+++ src/registry.ts
@@ -3,3 +3,4 @@ import { ModularLoadingScreen } from "./loadingScreen";
 export const PolyVersion = "0.5.1";
 export const ActiveLoaderRegistry = {};
 export const ActiveLoadingScreen = new ModularLoadingScreen();
+ActiveLoadingScreen.destroy(); // temp
